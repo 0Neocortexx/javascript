9917ace815f6d1dc0436fce59ffc23f0d91e3d35
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2022-09-17 02:05:10 -0300

    insercao de typescript

---
+++ typescript/projeto1/dist/js/index.js
@@ -0,0 +1,98 @@
+"use strict";
+// string, boolean, number, ...
+let x = 10;
+x = 12;
+console.log(x);
+// inferencia x annotation
+// inferencia (Tipagem menos explicita)
+let y = 12;
+// y = "teste";
+// annotation (Tipagem mais explicita)
+let z = 12;
+// Tipos básicos 
+let firstName = "Matheus";
+let age = 30;
+const idAdmin = true;
+// String != string
+console.log(typeof firstName);
+firstName = "João";
+console.log(firstName);
+// object
+const myNumbers = [1, 2, 3];
+console.log(myNumbers);
+console.log(myNumbers.length);
+console.log(firstName.toUpperCase());
+// Adicionar elemento a uma array
+myNumbers.push(5);
+console.log(myNumbers);
+// Tuplas 
+let myTuple;
+// ERRADO myTuple = [true, false, false];
+myTuple = [5, "asasas", ["asdasd", "adasd", "adsad"]];
+myTuple.push("dog amarelo", 34);
+console.log(myTuple);
+// Object literals -> {prop : value}
+const user = {
+    name: "Pedro",
+    age: 12
+};
+console.log(user);
+console.log(user.name);
+// any
+let a = 0;
+a = "teste";
+a = true;
+a = [];
+// union type
+let id = "10";
+id = 200;
+id = "200";
+const userId = 10;
+const productId = "00001";
+const shirId = 123;
+// enum
+// tamanho de roupas (size: Médio, size: Pequeno)
+var Size;
+(function (Size) {
+    Size["P"] = "Pequena ";
+    Size["M"] = "Media";
+    Size["G"] = "Grande";
+})(Size || (Size = {}));
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M,
+};
+console.log(camisa);
+// litaral types
+let teste;
+// teste = "outrovalor";
+teste = "autenticado";
+teste = null;
+// funcoes
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(12, 34));
+// console.log(sum("23",23))
+function sayhelloTo(name) {
+    return `Hello ${name}`;
+}
+console.log(sayhelloTo("Jonathan"));
+// void = função sem retorno
+function logger(msg) {
+    console.log(msg);
+}
+logger("teste!");
+function greeting(name, greet) {
+    if (greet) {
+        console.log(`Olá ${greet} ${name}`);
+    }
+    else {
+        console.log(`Olá ${name}`);
+    }
+}
+greeting("Jośe");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
---
+++ typescript/projeto1/src/index.ts
@@ -0,0 +1,126 @@
+
+// string, boolean, number, ...
+let x:number = 10;
+
+x = 12;
+
+console.log(x);
+
+// inferencia x annotation
+
+// inferencia (Tipagem menos explicita)
+let y  = 12;
+// y = "teste";
+
+// annotation (Tipagem mais explicita)
+let z: number = 12;
+
+// Tipos básicos 
+let firstName: string = "Matheus";
+let age: number = 30;
+const idAdmin: boolean = true;
+
+// String != string
+console.log(typeof firstName);
+firstName = "João";
+console.log(firstName);
+
+// object
+const myNumbers: number[] = [1,2,3]
+console.log(myNumbers);
+console.log(myNumbers.length);
+console.log(firstName.toUpperCase())
+// Adicionar elemento a uma array
+myNumbers.push(5);
+console.log(myNumbers);
+
+// Tuplas 
+let myTuple: [number, string,string[]];
+// ERRADO myTuple = [true, false, false];
+myTuple = [5, "asasas", ["asdasd", "adasd","adsad"]];
+myTuple.push("dog amarelo",34);
+console.log(myTuple)
+
+// Object literals -> {prop : value}
+const user: {name:string, age:number} = {
+    name: "Pedro",
+    age: 12
+}
+console.log(user);
+console.log(user.name);
+
+// any
+let a:any = 0
+a = "teste";
+a = true;
+a = [];
+
+// union type
+let id:string | number = "10"
+id = 200;
+id = "200";
+// id = True
+
+// type alias
+type myIdTipe = number | string
+const userId: myIdTipe = 10;
+const productId: myIdTipe = "00001";
+const shirId: myIdTipe = 123;
+
+// enum
+// tamanho de roupas (size: Médio, size: Pequeno)
+enum Size {
+    P = "Pequena ",
+    M = "Media",
+    G = "Grande",
+}
+const camisa = {
+    name: "Camisa gola V",
+    size: Size.M,
+}
+console.log(camisa);
+
+// litaral types
+let teste: "autenticado" | null;
+// teste = "outrovalor";
+teste = "autenticado";
+teste = null;
+
+// funcoes
+function sum(a: number,b:number ) {
+    return a + b
+}
+console.log(sum(12,34))
+// console.log(sum("23",23))
+
+function sayhelloTo(name: string): string {
+    return `Hello ${name}`;
+}
+console.log(sayhelloTo("Jonathan"));
+
+// void = função sem retorno
+function logger(msg:string): void {
+    console.log(msg)
+}
+logger("teste!");
+
+function greeting(name:string, greet?:string) {
+    if(greet) {
+        console.log(`Olá ${greet} ${name}`);
+    } else {
+        console.log(`Olá ${name}`);
+    }
+}
+greeting("Jośe");
+
+// In tsconfig.json exists removeComments: remove todos os comentários
+
+// interface
+interface MathFunctionParams{
+    n1 : number,
+    n2 : number
+}
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1:1 , n2:2 }));
