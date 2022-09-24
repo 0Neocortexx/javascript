5d7db1a08ec2ae665e95a01ee4edc22e7347cc12
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2022-09-24 12:25:57 -0300

    push interno

--- nodejs/projeto1/src/index.ts
+++ nodejs/projeto1/src/index.ts
@@ -1,6 +1,2 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080); 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
