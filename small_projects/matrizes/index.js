a3ea88e971d19e156d2c8b07512978f0c14de95d
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2022-10-21 23:23:24 -0300

    implementacao de matrizes

---
+++ small_projects/matrizes/index.js
@@ -0,0 +1,17 @@
+// Método 1
+    // Criar o Array com os elementos direto
+    // var matriz = [[1,2,3], [4,5,6], [7,8,9]];
+
+// Metodo 2
+    // Criar a matriz vazia
+    // var matriz = new Array(3);
+    // Adicionar os elementos
+    // matriz[0] = ['cachorro', 'pato', 'ganso']
+    // matriz[1] = [1 , 2 , 3]
+    // matriz[2] = [false, true, false]
+
+// Obs: podemos também criar e declarar uma matriz com elementos nulos e de qualquer tamanho de linhas ou colunas:
+    var matriz = new Array(2);
+    // Matriz dentro de matriz
+    matriz[0] = new Array(3);
+console.log(matriz)
